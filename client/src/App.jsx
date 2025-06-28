c139510689e1dcee4e504ec0c02381aa151fb420
Prayer07 <ezenwaokechukwu3@gmail.com>
2025-06-28 17:41:39 +0100

    added opay to the base route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
   return (
     <>
     <Routes>
-      <Route path='/' element={<Homepage/>} />
+      <Route path='/opay' element={<Homepage/>} />
       <Route path='/login' element={<Login/>} />
       <Route path='/signup' element={<Signup/>} />
 
