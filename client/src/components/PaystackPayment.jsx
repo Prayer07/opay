bd7daa264c56db1320ef66bbd4b1217f11fdbfb8
Prayer07 <ezenwaokechukwu3@gmail.com>
2025-06-28 15:18:42 +0100

    removed everything containing paystack

--- client/src/components/PaystackPayment.jsx
+++ client/src/components/PaystackPayment.jsx
@@ -1,29 +1,29 @@
-import React from 'react'
-import {PaystackButton} from "react-paystack"
+// import React from 'react'
+// import {PaystackButton} from "react-paystack"
 
-import dotenv from "dotenv"
-dotenv.config()
+// import dotenv from "dotenv"
+// dotenv.config()
 
 
 
-function PaystackPayment({amount, email, phone, onSuccess}) {
+// function PaystackPayment({amount, email, phone, onSuccess}) {
 
-  const publicKey = (process.env.PAYSTACK_TEST_PUBLIC_KEY)
-  const amountInKobo = amount * 100
+//   const publicKey = (process.env.PAYSTACK_TEST_PUBLIC_KEY)
+//   const amountInKobo = amount * 100
 
-  const componentProps = {
-    email,
-    amount: amountInKobo,
-    metadata: {
-      phone,
-    },
-    publicKey, 
-    text: "Pay now",
-    onSuccess: onSuccess,
-    onClose: () => alert("Transaction was not completed")
-  };
+//   const componentProps = {
+//     email,
+//     amount: amountInKobo,
+//     metadata: {
+//       phone,
+//     },
+//     publicKey, 
+//     text: "Pay now",
+//     onSuccess: onSuccess,
+//     onClose: () => alert("Transaction was not completed")
+//   };
 
-  return <PaystackButton {...componentProps} />
-}
+//   return <PaystackButton {...componentProps} />
+// }
 
-export default PaystackPayment
+// export default PaystackPayment
--- server/routes/paystack.route.js
+++ server/routes/paystack.route.js
@@ -1,53 +1,53 @@
-// routes/paystack.route.js
-import express from "express";
-import axios from "axios";
-import dotenv from "dotenv";
-dotenv.config();
+// // routes/paystack.route.js
+// import express from "express";
+// import axios from "axios";
+// import dotenv from "dotenv";
+// dotenv.config();
 
-const router = express.Router();
+// const router = express.Router();
 
-router.post("/initialize-payment", async (req, res) => {
-  const { email, amount } = req.body;
+// router.post("/initialize-payment", async (req, res) => {
+//   const { email, amount } = req.body;
 
-  try {
-    const response = await axios.post(
-      "https://api.paystack.co/transaction/initialize",
-      {
-        email,
-        amount: amount * 100, // in kobo
-        callback_url: "http://localhost:5173/verify-payment", // your frontend
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${process.env.PAYSTACK_SECRET_KEY}`,
-        },
-      }
-    );
+//   try {
+//     const response = await axios.post(
+//       "https://api.paystack.co/transaction/initialize",
+//       {
+//         email,
+//         amount: amount * 100, // in kobo
+//         callback_url: "http://localhost:5173/verify-payment", // your frontend
+//       },
+//       {
+//         headers: {
+//           Authorization: `Bearer ${process.env.PAYSTACK_SECRET_KEY}`,
+//         },
+//       }
+//     );
 
-    res.json(response.data);
-  } catch (error) {
-    res.status(500).json({ message: "Failed to initialize payment" });
-  }
-});
+//     res.json(response.data);
+//   } catch (error) {
+//     res.status(500).json({ message: "Failed to initialize payment" });
+//   }
+// });
 
-router.get("/verify/:reference", async (req, res) => {
-  const reference = req.params.reference;
+// router.get("/verify/:reference", async (req, res) => {
+//   const reference = req.params.reference;
 
-  try {
-    const response = await axios.get(
-      `https://api.paystack.co/transaction/verify/${reference}`,
-      {
-        headers: {
-          Authorization: `Bearer ${process.env.PAYSTACK_SECRET_KEY}`,
-        },
-      }
-    );
+//   try {
+//     const response = await axios.get(
+//       `https://api.paystack.co/transaction/verify/${reference}`,
+//       {
+//         headers: {
+//           Authorization: `Bearer ${process.env.PAYSTACK_SECRET_KEY}`,
+//         },
+//       }
+//     );
 
-    res.json(response.data);
-  } catch (err) {
-    res.status(500).json({ message: "Verification failed" });
-  }
-});
+//     res.json(response.data);
+//   } catch (err) {
+//     res.status(500).json({ message: "Verification failed" });
+//   }
+// });
 
 
-export default router;
+// export default router;
